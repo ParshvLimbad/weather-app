efb212312bfcac9d338027b2a8f6a9969d5b8d9f
Parshv Limbad <103508696+ParshvLimbad@users.noreply.github.com>
2024-05-25 16:03:48 +0530

    Add styles and API integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,12 @@ import WbSunnyIcon from "@mui/icons-material/WbSunny";
 import ThunderstormIcon from "@mui/icons-material/Thunderstorm";
 import AcUnitIcon from "@mui/icons-material/AcUnit";
 import WaterDropIcon from "@mui/icons-material/WaterDrop";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 let WEATHER_API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
 
 export default function Home() {
-  const [place, setPlace] = useState("Mexico");
+  const [place, setPlace] = useState("Mumbai");
   const [placeData, setPlaceData] = useState<any>(null);
   const currenTime = new Date().toLocaleTimeString([], {
     hour: "2-digit",
@@ -42,10 +43,13 @@ export default function Home() {
   return (
     <>
       <main className="flex justify-center items-center h-screen">
-        <div className="h-[500px] w-[370px] flex justify-center bg-red-400 rounded-lg bg-gradient-to-t from-custom-blue-start to-custom-blue-end p-10">
+        <div className="h-[500px] w-[370px] flex flex-col rounded-lg bg-gradient-to-t from-custom-blue-start to-custom-blue-end p-10 justify-center">
+          <h1 className="text-center mb-8 text-3xl font-semibold text-[#66DFFF]">
+            WeatherMan
+          </h1>
           <div
             id="search"
-            className="bg-white h-[10%] flex items-center justify-between w-[280px] pr-5 pl-5 rounded-full"
+            className="bg-white self-center h-[10%] mb-8 flex items-center justify-between w-[280px] pr-5 pl-5 rounded-full"
           >
             <input
               className="outline-none w-[200px]"
@@ -53,31 +57,94 @@ export default function Home() {
               onChange={(e) => setPlace(e.target.value)}
             />
             <button onClick={getWeatherData} className="">
-              <SearchIcon />
+              <SearchIcon style={{ color: "" }} />
             </button>
           </div>
-        </div>
-        {placeData && (
-          <div>
-            <div>
-              <div id="section1"></div>
-              <div id="section2">
-                {placeData.weather[0].main === "Clouds" && <FilterDramaIcon />}
-                {placeData.weather[0].main === "Haze" && <DehazeIcon />}
-                {placeData.weather[0].main === "Smoke" && <VapingRoomsIcon />}
-                {placeData.weather[0].main === "Clear" && <WbSunnyIcon />}
+          <div className="flex flex-row items-center justify-between">
+            {placeData && (
+              <div>
+                <p id="cityName" className="font-normal text-[40px] text-white">
+                  {placeData?.name}
+                </p>
+              </div>
+            )}
+            {placeData && (
+              <div id="section11" className="flex flex-row">
+                {placeData.weather[0].main === "Clouds" && (
+                  <FilterDramaIcon
+                    style={{ height: "55px", width: "auto", color: "white" }}
+                  />
+                )}
+                {placeData.weather[0].main === "Haze" && (
+                  <DehazeIcon
+                    style={{ height: "55px", width: "auto", color: "white" }}
+                  />
+                )}
+                {placeData.weather[0].main === "Smoke" && (
+                  <VapingRoomsIcon
+                    style={{ height: "55px", width: "auto", color: "white" }}
+                  />
+                )}
+                {placeData.weather[0].main === "Clear" && (
+                  <WbSunnyIcon
+                    style={{ height: "55px", width: "auto", color: "white" }}
+                  />
+                )}
                 {placeData.weather[0].main === "Thunderstorm" && (
                   <ThunderstormIcon />
                 )}
-                {placeData.weather[0].main === "Snow" && <AcUnitIcon />}
-                {placeData.weather[0].main === "Rain" && <WaterDropIcon />}
+                {placeData.weather[0].main === "Snow" && (
+                  <AcUnitIcon
+                    style={{ height: "55px", width: "auto", color: "white" }}
+                  />
+                )}
+                {placeData.weather[0].main === "Rain" && (
+                  <WaterDropIcon
+                    style={{ height: "55px", width: "auto", color: "white" }}
+                  />
+                )}
               </div>
+            )}
+          </div>
+          <div className="text-end mr-0.5 mt-[-8px]">
+            {placeData && (
+              <div>
+                <p id="weatherType" className="font-normal text-white">
+                  {placeData?.weather[0].main}
+                </p>
+              </div>
+            )}
+          </div>
+          <div className="flex flex-row-reverse justify-between mt-7 mb-[53px]">
+            <div>
+              {placeData && (
+                <div className="text-right text-white">
+                  <p className="text-xl">Temp</p>
+                  <p className="text-[27px] font-medium">
+                    {(placeData?.main.temp - 273.15).toFixed(1)}
+                    <span>°C</span>
+                  </p>
+                </div>
+              )}
             </div>
-            <div id="timediv">
-              <p id="time">{currenTime}</p>
+            <div>
+              {placeData && (
+                <div id="timediv" className="text-white">
+                  <p className="text-xl">Time</p>
+                  <p id="time" className="text-[27px] font-medium">
+                    {currenTime}
+                  </p>
+                </div>
+              )}
             </div>
           </div>
-        )}
+          <div className="flex flex-col justify-center items-center scale-90 text-white">
+            <a target="_blank" href="https://github.com/ParshvLimbad">
+              <GitHubIcon style={{ height: "25px", width: "auto" }} />
+            </a>
+            <p>By Parshv Limbad</p>
+          </div>
+        </div>
       </main>
     </>
   );
